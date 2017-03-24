ca3159ffa30084f7a6f58b536d2b0259c5ea56d4
Asmi0816 <asmi0816@740s-pl-114.local>
2017-03-24 13:07:11 -0600

    Finished making the score work

--- src/roshambo/controller/Controller.java
+++ src/roshambo/controller/Controller.java
@@ -44,6 +44,7 @@ public class Controller
 			else if(gameChoice.equalsIgnoreCase("Ro") && computer == 1)
 			{
 				System.out.println("Sorry you threw Ro(Rock) and the computer threw Sham(Paper)");
+				score -= 1;
 			}
 			else if(gameChoice.equalsIgnoreCase("Ro") && computer == 0)
 			{
@@ -57,6 +58,7 @@ public class Controller
 			else if(gameChoice.equalsIgnoreCase("Sham") && computer == 2)
 			{
 				System.out.println("Sorry you threw Sham(Paper) and the computer threw Bo(Scissors)");
+				score -= 1;
 			}
 			else if(gameChoice.equalsIgnoreCase("Sham") && computer == 1)
 			{
@@ -70,12 +72,13 @@ public class Controller
 			else if(gameChoice.equalsIgnoreCase("Bo") && computer == 2)
 			{
 				System.out.println("Sorry you threw Bo(Scissors) and the computer threw Ro(Rock)");
+				score -= 1;
 			}
 			else if(gameChoice.equalsIgnoreCase("Bo") && computer == 0)
 			{
 				System.out.println("Tie you threw Bo(Scissors) and the computer threw Bo(Scissors)");
 			}
-			System.out.println("\n");
+			System.out.println("You have a score of "+ score + "\n");
 			startUp();
 		}
 		
